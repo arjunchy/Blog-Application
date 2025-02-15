e9922bfcf4563440480c05c254e3c9d122f26a92
arjunchy <arjunchaudhary477058@gmail.com>
2025-02-15 19:01:09 +0530

    bycrypt

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,13 +1,16 @@
 import User from '../model/user.js';
+import bcrypt from 'bcrypt';
+export const signupUser = async (req, resp) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
-export const signupUser = async (req,resp) =>{
-    try{
-       const user = req.body;
-       const newUser = new User(user);
-       await newUser.save();
+        const user = { username: req.body.username, name: req.body.name, password: hashedPassword };
 
-       return resp.status(200).json({msg : 'User created successfully'});
-    }catch(err){
-        return resp.status(500).json({msg : err.message});
+        const newUser = new User(user);
+        await newUser.save();
+
+        return resp.status(200).json({ msg: 'User created successfully' });
+    } catch (err) {
+        return resp.status(500).json({ msg: err.message });
     }
 }
