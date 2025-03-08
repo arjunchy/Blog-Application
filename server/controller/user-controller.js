4f81ccdc274210dc95f12f4b664260b3148f0e4b
arjunchy <arjunchy>
2025-03-08 14:54:17 +0530

    jwt authentication

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -2,16 +2,19 @@ import User from '../model/user.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-import Token from '../model/token.js'
+import Token from '../model/token.js';
 
 dotenv.config();
-
 
 export const signupUser = async (req, resp) => {
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
-        const user = { username: req.body.username, name: req.body.name, password: hashedPassword };
+        const user = { 
+            username: req.body.username, 
+            name: req.body.name, 
+            password: hashedPassword 
+        };
 
         const newUser = new User(user);
         await newUser.save();
@@ -20,33 +23,40 @@ export const signupUser = async (req, resp) => {
     } catch (err) {
         return resp.status(500).json({ msg: err.message });
     }
-}
-
+};
 
 export const loginUser = async (req, resp) => {
-    const user = await User.findOne({ username: req.body.username });
-    if (!user) {
-        return resp.status(400).json({
-            msg: 'Invalid username or password'
-        })
-    }
-
     try {
-        let match = await bcrypt.compare(req.body.password, user.password);
-        if (match) {
-            const accessToken = jwt.sign({ id: user._id, username: user.username }, process.env.ACCESS_SECRET_KEY, { expiresIn: '15m' });
-            const refreshToken = jwt.sign({ id: user._id, username: user.username }, process.env.REFRESH_SECRET_KEY);
-
-            const newToken = new Token({ token: refreshToken })
-            await newToken.save();
-
-            return resp.status(200).json({ accessToken: accessToken, refreshToken: refreshToken, name: user.name, username: user.username });
-
-        } else {
-            return resp.status(400).json({ msg: "Password does not match" })
+        const user = await User.findOne({ username: req.body.username });
+        if (!user) {
+            return resp.status(400).json({ msg: 'Invalid username or password' });
         }
-    } catch (err) {
-        return resp.status(500).json({ msg: "Error while logging in" })
-    }
 
-}
+        const match = await bcrypt.compare(req.body.password, user.password);
+        if (!match) {
+            return resp.status(401).json({ msg: 'Password does not match' });
+        }
+
+        const accessToken = jwt.sign(
+            { id: user._id, username: user.username }, 
+            process.env.ACCESS_SECRET_KEY, 
+            { expiresIn: '15m' }
+        );
+
+        const refreshToken = jwt.sign(
+            { id: user._id, username: user.username }, 
+            process.env.REFRESH_SECRET_KEY
+        );
+
+        const newToken = new Token({ token: refreshToken });
+        await newToken.save();
+
+        return resp.status(200).json({
+            msg: 'Login successful',
+            accessToken,
+            refreshToken
+        });
+    } catch (err) {
+        return resp.status(500).json({ msg: 'Error while logging in', error: err.message });
+    }
+};
