4405c307711088a0e7f0bf6841eb130943f5ecc0
arjunchy <arjunchaudhary477058@gmail.com>
2025-04-10 10:22:55 +0530

    jwt-controller

--- server/controller/jwt-controller.js
+++ server/controller/jwt-controller.js
@@ -3,18 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-<<<<<<< HEAD
-export const authenticateToken = (request, response, next) =>{
-    const authHeader = request.header['authorization']
-    const token = authHeader && authHeader.split('')[1];
-
-    if(token == null){
-        return response.status(401).json({ msg: 'token is missing'});
-    }
-
-    jwt.verify(token, process.env.ACCESS_SECRET_KEY, (err, user) => {
-        if(err) {
-=======
 export const authenticateToken = (request, response, next) => {
     const authHeader = request.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
@@ -25,15 +13,9 @@ export const authenticateToken = (request, response, next) => {
 
     jwt.verify(token, process.env.ACCESS_SECRET_KEY, (err, user) => {
         if (err) {
->>>>>>> ac3876e (removed imageupload)
             return response.status(403).json({ msg: 'Invalid token' });
         }
         request.user = user;
         next();
-<<<<<<< HEAD
-    })
-}
-=======
     });
 };
->>>>>>> ac3876e (removed imageupload)
